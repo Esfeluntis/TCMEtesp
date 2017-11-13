9f3d20f212741c44331a52a253623a1b3929f7bd
Marco <marcoacop@live.com>
2017-11-13 12:11:47 -0200

    Página de registro de glicemia adicionada - não funcional

---
+++ ASPVidaDiabetica/ASPVidaDiabetica/pgs/RegistroGlicemia.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ASPVidaDiabetica.pgs
+{
+    public partial class RegistroGlicemia : System.Web.UI.Page
+    {
+        DataTable dt = new DataTable();
+        ClasseConexao con = new ClasseConexao();
+        Share sh = new Share();
+        
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnFinalBen_Click(object sender, EventArgs e)
+        {
+            Session["id_ben"] = "1";
+            dt = new DataTable();
+            con = new ClasseConexao();
+            dt = con.executarSQL("update tblRegGlicemia set " + ddlOca.SelectedValue + " = " + txtValor.Text + " where data = '" + txtData.Text + "' and idBeneficiado = 1");
+
+        }
+    }
+}
